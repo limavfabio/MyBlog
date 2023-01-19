2f0aaefde9ab97e82452c55264c1e1e2150928fb
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-19 17:45:21 -0300

    Fix references to author_id and generate schema

--- db/migrate/20230119181437_create_posts.rb
+++ db/migrate/20230119181437_create_posts.rb
@@ -9,7 +9,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_reference :posts, :author, foreign_key: {to_table: users}
+    add_reference :posts, :author, foreign_key: {to_table: 'users'}
 
   end
 end
